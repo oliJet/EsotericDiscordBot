3ef2a8546de05b97e2f551de882cd28764c4dc81
Oliver Jet <oliver.jet.reid@gmail.com>
2020-04-27 13:53:47 +1000

    add launchsettings to ignore

--- EsotericDiscordBot/Program.cs
+++ EsotericDiscordBot/Program.cs
@@ -1,56 +1,143 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace EsotericDiscordBot 
 {
-	public class Program
-	{
-		private DiscordSocketClient _client;
+    public class Program
+    {
 
-		public static void Main(string[] args)
-			=> new Program().MainAsync().GetAwaiter().GetResult();
+        static void Main(string[] args)
+        {
+            // Call the Program constructor, followed by the 
+            // MainAsync method and wait until it finishes (which should be never).
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
 
-		public async Task MainAsync()
-		{
-			_client = new DiscordSocketClient();
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private readonly IServiceProvider _services;
 
-			_client.Log += Log;
-			_client.MessageReceived += MessageReceived;
+        private Program()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+            });
 
-			// Remember to keep token private or to read it from an 
-			// external source! In this case, we are reading the token 
-			// from an environment variable.
-			await _client.LoginAsync(TokenType.Bot,
-				Environment.GetEnvironmentVariable("DiscordToken"));
-			await _client.StartAsync();
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                LogLevel = LogSeverity.Info,
+                CaseSensitiveCommands = false,
+            });
 
+            // Subscribe the logging handler to both the client and the CommandService.
+            _client.Log += Log;
+            _commands.Log += Log;
 
-			// Block this task until the program is closed.
-			await Task.Delay(-1);
-		}
+            // Setup your DI container.
+            _services = ConfigureServices();
+        }
 
-		private Task Log(Discord.LogMessage msg)
-		{
-			Console.WriteLine(msg.ToString());
-			return Task.CompletedTask;
-		}
+        // If any services require the client, or the CommandService, or something else you keep on hand,
+        // pass them as parameters into this method as needed.
+        private static IServiceProvider ConfigureServices()
+        {
+            var map = new ServiceCollection();
+            // Repeat this for all the service classes
+            // and other dependencies that your commands might need.
+            //.AddSingleton(new CommandHandler());
 
-		//TODO: Update checking username to check for specific user role such as Botdev
-		private async Task MessageReceived(SocketMessage message)
-		{
-			if (message.Content.ToLower() == "!ping" && message.Author.Username == "crybaby claps")
-			{
-				await message.Channel.SendMessageAsync("Pings are working, your command handler is probably broken you scrub");
-			}
+            return map.BuildServiceProvider();
+        }
 
-			if (message.Content.ToLower() == "!ping")
-			{
-				await message.Channel.SendMessageAsync("Pong!");
-			}
+        private static Task Log(LogMessage message)
+        {
+            switch (message.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
+            Console.ResetColor();
 
-		}
+            return Task.CompletedTask;
+        }
 
-	}
+        private async Task MainAsync()
+        {
+            // Centralize the logic for commands into a separate method.
+            await InitCommands();
+
+            // Login and connect.
+            await _client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+
+            // Wait infinitely so your bot actually stays connected.
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private async Task InitCommands()
+        {
+            // Either search the program and add all Module classes that can be found.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            //optional add Modules manually:
+            //await _commands.AddModuleAsync<InfoModule>(_services);
+
+            // Subscribe a handler to see if a message invokes a command.
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // We don't want the bot to respond to itself or other bots.
+            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            // Replace the '!' with whatever character
+            // you want to prefix your commands with.
+            // Uncomment the second half if you also want
+            // commands to be invoked by mentioning the bot instead.
+            if (msg.HasCharPrefix('!', ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */)
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(_client, msg);
+
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed successfully).
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed.
+                // This does not catch errors from commands with 'RunMode.Async',
+                // subscribe a handler for '_commands.CommandExecuted' to see those.
+                //if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                //    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
 }
